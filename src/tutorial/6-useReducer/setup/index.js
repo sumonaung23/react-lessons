48ef52dac12fb73a0dce73d383ebbd56623ecc78
Su Mon Aung <sumonaung575@gmail.com>
2023-08-07 22:44:09 +0630

    created reducer.js and moved the function into it then wrapped up the lesson

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -2,40 +2,7 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
-const reducer = (state, action) => {
-  if(action.type === 'ADD_ITEM'){
-    const newPeople = [...state.people, action.payLoad]
-    return {
-      ...state, 
-      people:newPeople,
-      isModalOpen:true,
-      modalContent:'item added'
-    };
-  }
-  if(action.type === 'NO_VALUE'){
-      return {
-        ...state,
-        isModalOpen: true,
-        modalContent: 'please enter value'
-      };
-    }
-    if(action.type === 'CLOSE_MODAL') {
-      return {
-        ...state,
-        isModalOpen: false
-      }
-    }
-    if(action.type === 'REMOVE_ITEM') {
-      const newPeople = state.people.filter((person) => person.id !== action.payLoad);
-      return {
-        ...state,
-        people: newPeople
-      };
-    }
-
-  // if none of them matches, throw the error
-  throw new Error('no matching action type');
-}
+import { reducer } from './reducer';
 const defaultState = {
   people: [],
   isModalOpen: false,
