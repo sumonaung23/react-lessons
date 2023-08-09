d5be486efcca6d6d126d978f1ce0804cb68e9d6d
Su Mon Aung <sumonaung575@gmail.com>
2023-08-09 21:59:20 +0630

    completed customHook in react

--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -6,20 +6,7 @@ import { useFetch } from './2-useFetch'
 const url = 'https://course-api.com/javascript-store-products'
 
 const Example = () => {
-  const [loading, setLoading] = useState(true)
-  const [products, setProducts] = useState([])
-
-  const getProducts = async () => {
-    const response = await fetch(url)
-    const products = await response.json()
-    setProducts(products)
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    getProducts()
-  }, [url])
-  console.log(products)
+  const {loading, products} = useFetch(url);
   return (
     <div>
       <h2>{loading ? 'loading...' : 'data'}</h2>
